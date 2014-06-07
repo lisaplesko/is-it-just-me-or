ece94f6c78d81224cc4b093e434e108a6d2c8b48
Mike Talley <miketalley@hotmail.com>
2014-06-07 16:24:53 -0400

    Added Category table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :categories
+
   resources :comments
 
   resources :posts
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607200522) do
+ActiveRecord::Schema.define(version: 20140607202412) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.string   "body"
