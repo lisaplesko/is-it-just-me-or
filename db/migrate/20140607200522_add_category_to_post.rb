b6d64a99c00c4d044f78ff5470d6b32c9e8f0fd2
Mike Talley <miketalley@hotmail.com>
2014-06-07 18:17:26 -0400

    Modified migration add category to post

--- db/migrate/20140607200522_add_category_to_post.rb
+++ db/migrate/20140607200522_add_category_to_post.rb
@@ -1,16 +1,7 @@
 class AddCategoryToPost < ActiveRecord::Migration
   def change
     change_table :posts do |t|
-      reversible do |dir|
-        dir.up {
-          t.remove :category
-          t.references :category
-        }
-        dir.down {
-          t.remove :category_id
-          t.string :category
-        }
-      end
+        t.references :category
     end
   end
 end
