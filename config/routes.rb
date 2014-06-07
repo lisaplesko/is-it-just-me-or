244dec0d0a9c4640377eda7c33548fc078bc8c5b
Mike Talley <miketalley@hotmail.com>
2014-06-07 15:46:23 -0400

    Generated scaffold for Comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+
   resources :posts
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607193531) do
+ActiveRecord::Schema.define(version: 20140607194454) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
