62fa1cbb56545c4b6ff273ee10010c5c78dc2312
Mike Talley <miketalley@hotmail.com>
2014-06-08 15:36:11 -0400

    Added method to check if category exists

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,17 +1,21 @@
 class Category < ActiveRecord::Base
     has_many :posts
 
-    self.count_posts
+    def self.count_posts
       Post.all.where(category_id: params[:id]).count
     end
 
-    self.count_comments
+    def self.count_comments_on_posts
       @comment_count = 0
       @cat_posts = Post.all.where(category_id: params[:id])
       @cat_posts.each do |post|
         @comment_count += post.count_comments
       end
       return @comment_count
+    end
+
+    def exists?(category_name)
+      !Category.find(name: category_name).nil?
     end
 
 end
