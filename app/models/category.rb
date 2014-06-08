6608ab041aba387560dab0d4a25da2586334f9a9
Mike Talley <miketalley@hotmail.com>
2014-06-08 14:59:31 -0400

    Added count methods for comments and posts

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,17 @@
 class Category < ActiveRecord::Base
     has_many :posts
+
+    self.count_posts
+      Post.all.where(category_id: params[:id]).count
+    end
+
+    self.count_comments
+      @comment_count = 0
+      @cat_posts = Post.all.where(category_id: params[:id])
+      @cat_posts.each do |post|
+        @comment_count += post.count_comments
+      end
+      return @comment_count
+    end
+
 end
