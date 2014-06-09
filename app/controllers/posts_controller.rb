0169a4bacdbefbf648ab5dc0754d2638a7659869
Ethan Gould <ethan.g.907@gmail.com>
2014-06-09 13:52:16 -0400

    set posts index to be specific to the current_users posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = current_user.posts.all
     # @user_posts = current_user.posts.all
   end
 
