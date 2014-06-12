37263750faf19deda8a2166e59fa84be77c7c0fc
Lisa Plesko <plesko.l@gmail.com>
2014-06-12 14:55:23 -0400

    Refactored Scroller.js

--- app/assets/javascripts/scroller.js
+++ app/assets/javascripts/scroller.js
@@ -1,38 +1,40 @@
+var BlogApp = BlogApp || {};
+
 $(document).ready(function(){
 
-  $('#scroll-section').scroll(updateText);
+  $('#scroll-section').scroll(BlogApp.updateText);
 
   $.ajax({
     url: '/categories.json'
   })
-  .done(calculateTop8);
+  .done(BlogApp.calculateTop8);
 });
 
-var insertCategorySection = function(posts, count){
+BlogApp.insertCategorySection = function(posts, count){
   $('#scroll-section').append("<section class='scrollback' id='scroll" + count + "' data-type='background' data-speed='10'>");
   for(var i = 0; i < posts.length; i++){
-    $('#scroll' + count).append(generatePostHTML(posts[i]));
+    $('#scroll' + count).append(BlogApp.generatePostHTML(posts[i]));
     $('#scroll').append("<div class='.row'><div class='span12'><hr></div></div>");
   }
 };
 
-var generatePostHTML = function(post){
+BlogApp.generatePostHTML = function(post){
   return HandlebarsTemplates['posts/show'](post);
 };
 
-var insertScrollBreakSection = function(category){
+BlogApp.insertScrollBreakSection = function(category){
   var scrollbreak_html = HandlebarsTemplates['scroller/break'](category);
   $('#scroll-section').append(scrollbreak_html);
 };
 
-var appendTop8 = function(topCategories){
+BlogApp.appendTop8 = function(topCategories){
   for(var i = 0; i < topCategories.length; i++){
-    insertScrollBreakSection(topCategories[i]);
-    insertCategorySection(getTop4Posts(topCategories[i]), i);
+    BlogApp.insertScrollBreakSection(topCategories[i]);
+    BlogApp.insertCategorySection(BlogApp.getTop4Posts(topCategories[i]), i);
   }
 };
 
-var calculateTop8 = function(categories){
+BlogApp.calculateTop8 = function(categories){
   categories.sort(function (a, b) {
     if (a.category_score < b.category_score){
       return 1;
@@ -43,10 +45,10 @@ var calculateTop8 = function(categories){
     }
   });
   categories.length = 8;
-  appendTop8(categories);
+  BlogApp.appendTop8(categories);
 };
 
-var getTop4Posts = function(category){
+BlogApp.getTop4Posts = function(category){
   var top4Posts = category.posts.sort(function (a, b) {
     if (a.post_score < b.post_score){
       return 1;
@@ -69,20 +71,20 @@ var getTop4Posts = function(category){
 //   .done(displayOne);
 // });
 
-var displayOne = function(categories){
-  insertScrollBreakSection(categories[0]);
-  insertCategorySection(categories[0], 0);
+BlogApp.displayOne = function(categories){
+  BlogApp.insertScrollBreakSection(categories[0]);
+  BlogApp.insertCategorySection(categories[0], 0);
 };
 
-var updateText = function(event){
+BlogApp.updateText = function(event){
   // The text on the page should stay put in center
   // this means it must scroll down for each scroll down
   event.preventDefault();
 
   var calcTop = function(){
-    var windowHeight = $(window).height();   // returns height of browser viewport
-    var documentHeight = $(document).height(); // returns height of HTML document
-    var scrollTop = $(window).scrollTop(); // return the number of pixels scrolled vertically
+    var windowHeight = $(window).height(),  // returns height of browser viewport
+        documentHeight = $(document).height(), // returns height of HTML document
+        scrollTop = $(window).scrollTop(); // return the number of pixels scrolled vertically
     return windowHeight - scrollTop;
   };
 
